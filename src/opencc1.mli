7cc1bfee1bf784da11f37bfb7c1973522b13ca9a
ZAN DoYe <357626+kandu@users.noreply.github.com>
2018-12-06 18:32:43 +0800

    fix typo

--- src/opencc1.mli
+++ src/opencc1.mli
@@ -8,20 +8,20 @@ type t
 (** the type of opencc instances *)
 
 val create : string -> t
-(** [Opencc.create config] creates a new opencc instance. [config] should be the path of the appropriate config file. *)
+(** [Opencc1.create config] creates a new opencc instance. [config] should be the path of the appropriate config file. *)
 
 val convert_utf8 : t -> string -> string
-(** [Opencc.convert_utf8 opencc text] returns the converted string. [text] is UTF-8 encoded input string. *)
+(** [Opencc1.convert_utf8 opencc text] returns the converted string. [text] is UTF-8 encoded input string. *)
 
 val error : unit -> string
-(** [Opencc.error ()] returns the last error message. *)
+(** [Opencc1.error ()] returns the last error message. *)
 
 (** {2 example}
 On a debian jessie x86_64 system, the source code below:
 {[
 let ()=
-  let opencc= Opencc.create "/usr/share/opencc/s2t.json" in
-  print_endline (Opencc.convert_utf8 opencc "汉字");;
+  let opencc= Opencc1.create "/usr/share/opencc/s2t.json" in
+  print_endline (Opencc1.convert_utf8 opencc "汉字");;
 ]}
 
 will print out {b 漢字}
